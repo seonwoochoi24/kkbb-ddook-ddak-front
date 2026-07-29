b325efe3d5ab324c315f30e0c665d84a35e2c9b7
최선우 <seonwoo-choi@choeseon-uui-MacBookPro.local>
2026-07-29 19:37:07 +0900

    [Fix] MainPage 대소문자 수정

--- src/router.jsx
+++ src/router.jsx
@@ -2,7 +2,7 @@
 
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.jsx";
-import MainPage from "./pages/MainPage.jsx";
+import MainPage from "./pages/Mainpage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -10,7 +10,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <MainPage />,
+        element: <Mainpage />,
       },
       {
         path: "*",
